51c2803c293e65286013e0aa57739d56f6da025c
Sarah <Sarah@HP-SARAHI>
2023-12-05 07:44:11 -0600

    Agregar archivos de proyecto.

---
+++ CountingSort.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Sort
+{
+    internal class CountingSort
+    {
+        public static int[] Sort(int[] inputArray)
+        {
+            // Encontrar el elemento máximo de inputArray.
+            int M = 0;
+            foreach (int num in inputArray)
+            {
+                if (num > M)
+                {
+                    M = num;
+                }
+            }
+
+            // Inicializar countArray con 0
+            int[] countArray = new int[M + 1];
+
+            // Asignar cada elemento de inputArray como un índice de countArray
+            foreach (int num in inputArray)
+            {
+                countArray[num]++;
+            }
+
+            // Calcular la suma prefija en cada índice de countArray
+            for (int i = 1; i < M + 1; i++)
+            {
+                countArray[i] += countArray[i - 1];
+            }
+
+            // Crear outputArray a partir de countArray
+            int[] outputArray = new int[inputArray.Length];
+
+            for (int i = inputArray.Length - 1; i >= 0; i--)
+            {
+                outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+                countArray[inputArray[i]]--;
+            }
+
+            return outputArray;
+        }
+
+        public static string[] SortStrings(string[] inputArray)
+        {
+            // Encontrar la cadena más larga en inputArray.
+            int maxLength = 0;
+            foreach (string str in inputArray)
+            {
+                if (str.Length > maxLength)
+                {
+                    maxLength = str.Length;
+                }
+            }
+
+            // Inicializar countArray con 0
+            int range = 256; // Considerando el rango ASCII extendido
+            int[][] countArray = new int[inputArray.Length][];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                countArray[i] = new int[range];
+            }
+
+            // Contar la frecuencia de cada carácter en cada string
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                foreach (char c in inputArray[i])
+                {
+                    countArray[i][c]++;
+                }
+            }
+
+            // Calcular la suma prefija en cada índice de countArray
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 1; j < range; j++)
+                {
+                    countArray[i][j] += countArray[i][j - 1];
+                }
+            }
+
+            // Crear outputArray a partir de countArray
+            string[] outputArray = new string[inputArray.Length];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                char[] sortedString = new char[maxLength];
+
+                for (int j = inputArray[i].Length - 1; j >= 0; j--)
+                {
+                    int index = --countArray[i][inputArray[i][j]];
+                    sortedString[index] = inputArray[i][j];
+                }
+
+                outputArray[i] = new string(sortedString);
+            }
+
+            return outputArray;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace Counting_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] inputArray = new int[10];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = rnd.Next(101); // Genera un número aleatorio entre 0 y 100
+            }
+
+            // Mostrar el array generado aleatoriamente
+            Console.WriteLine("Array generado aleatoriamente:");
+            foreach (int num in inputArray)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            // Iniciar el cronómetro para medir el tiempo
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Ordenar el array utilizando CountingSort
+            int[] outputArray = CountingSort.Sort(inputArray);
+
+            // Detener el cronómetro
+            stopwatch.Stop();
+
+            // Mostrar el array ordenado
+            Console.WriteLine("\nArray ordenado:");
+            foreach (int num in outputArray)
+            {
+                Console.Write(num + " ");
+            }
+
+            // Mostrar el tiempo transcurrido
+            Console.WriteLine($"\n\nTiempo transcurrido en ordenar: {stopwatch.Elapsed}");
+
+            // Permitir al usuario ingresar una cadena para ordenarla
+            Console.WriteLine("\nIngrese una cadena para ordenarla por caracteres ASCII:");
+            string userString = Console.ReadLine();
+
+            string[] userArray = new string[] { userString };
+            string[] sortedUserArray = CountingSort.SortStrings(userArray);
+
+            // Mostrar la cadena ordenada
+            Console.WriteLine("\nCadena ordenada:");
+            Console.WriteLine(sortedUserArray[0]);
+        }
+    }
+}
